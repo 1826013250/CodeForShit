e356b25fda367d233b89560e9d9881636b3eded5
David-123 <62735351+1826013250@users.noreply.github.com>
2023-11-18 17:49:49 +0800

    Add files via upload

---
+++ _Il判断回文字符串.cpp
@@ -0,0 +1,87 @@
+/*
+ * Author: David-123
+ * Find more at: https://github.com/1826013250/CodeForShit
+ * 此代码纯属为了整活，请勿模仿
+ * Don't try to imitate this dummy code. It doesn't desire your time.
+ */
+
+#include <iostream>
+#define IlIl using
+#define llII namespace
+#define IIII std
+#define llll__ int
+#define llll_I char
+#define llllII for
+#define llllII_ while
+#define llllI_ if
+#define llllll break
+#define llllIl return
+#define llll_llll cout
+#define llll_lllI cin
+#define llll__l__ false
+#define llll__I__ true
+#define IIllll getchar
+
+#define _ ;
+#define _I (
+#define _l )
+#define _II [
+#define _ll ]
+#define _III {
+#define _lll }
+#define __l =
+#define __Ill *
+#define __IlI +
+#define __IlI__ ++
+#define __IIl -
+#define __IIl__ --
+#define __l_l ?
+#define __l_I :
+#define __lll ==
+#define __lll_ !=
+#define __llI >=
+#define __llI_ <
+#define __IIIl <<
+#define __IIII >>
+
+#define _I_ll 100000
+#define _I_lI 0
+#define _I_Il 2
+#define l_lII '\n'
+#define I_lll "Yes"
+#define I_llI "No"
+#define I_IIIl '0'
+#define I_llll 10
+
+#define __IlllI__ main
+
+
+IlIl llII IIII _
+
+llll_I __I__ _II _I_ll _ll _
+llll__ __IllI__ _I _l _III
+	llll_I _l_ _
+	llll__ _lI_ __l _I_lI _
+	llllII_ _I _l_ __lll_ l_lII _l _III
+		_l_ __l IIllll _I _l _
+		__I__ _II _lI_ _ll __l _l_ _
+		_lI_ __IlI__ _
+	_lll
+	_lI_ __l _lI_ __IIl _I_Il _
+	llllII _I llll__ _llIll_ __l _lI_ _ _llIll_ __llI _I_lI _ _llIll_ __IIl__ _l _III
+		llllI_ _I __I__ _II _llIll_ _ll __lll_ __I__ _II _lI_ __IIl _llIll_ _ll _l
+			llllIl llll__l__ _
+	_lll
+	llllIl llll__I__ _
+_lll
+
+llll__ __IlllI__ _I _l _III
+	llll__ _III_ __l _I_lI _
+	llll_I _llll_ _
+	llllII_ _I _I _llll_ __l IIllll _I _l _l __lll_ l_lII _l
+		_III_ __l _III_ __Ill I_llll __IlI _llll_ __IIl I_IIIl _
+	
+	llllII _I llll__ _Il_ __l _I_lI _ _Il_ __llI_ _III_ _ _Il_ __IlI__ _l
+		llll_llll __IIIl _I __IllI__ _I _l __l_l I_lll __l_I I_llI _l __IIIl l_lII _
+	llllIl _I_lI _
+_lll
